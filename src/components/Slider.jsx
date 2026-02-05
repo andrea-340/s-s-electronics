51c7c53b8a3afa74b3d18176539469f9f9a0af42
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-02-05 12:21:09 +0100

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 
-const slides = [
-  "/2025-01-2722.jpg",
-  "/2025-01-272.jpg",
-  "/2025-01-27.jpg",
-  "/2025-01-272243.jpg",
-  "/unnamed.jpg",
-  "/unnamed2.jpg",
-];
+// Importa le immagini singolarmente (assicurati che il percorso sia corretto)
+import img1 from "./assets/2025-01-2722.jpg";
+import img2 from "./assets/2025-01-272.jpg";
+import img3 from "./assets/2025-01-27.jpg";
+import img4 from "./assets/2025-01-272243.jpg";
+import img5 from "./assets/unnamed.jpg";
+import img6 from "./assets/unnamed2.jpg";
+
+// Ora l'array contiene i riferimenti importati, non più stringhe fisse
+const slides = [img1, img2, img3, img4, img5, img6];
+
 export default function Slider() {
   const [current, setCurrent] = useState(0);
 

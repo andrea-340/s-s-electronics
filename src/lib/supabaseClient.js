6630c793a7361bdbfe9ae8589c449a43d614fa40
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-03-31 02:09:53 +0200

    Delete src directory

--- src/lib/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Inserisci i valori del tuo progetto Supabase
-const SUPABASE_URL = "https://qkkxavsjtbzhnsrxxnen.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_Hboz00Iq2THzP8v-l4c6mA_YuqZHhWW";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-
